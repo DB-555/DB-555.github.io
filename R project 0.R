7a0d88f5e12a2f80afbf874fe5bd57c3d20f9ce9
DB-555 <ditipriya.bhar@gmail.com>
2024-09-08 17:17:40 -0400

    Initial commit

--- R project 0.R
+++ R project 0.R
@@ -1,5 +1,21 @@
 
+# Project 0 #
+###############################################
+
+#Part 1
+# Setting up the computer environment 
 print(R.version.string)
 print(RStudio.Version()$version)
 
+
+sessionInfo()
+# Know your git version
+system("git --version")
+
+usethis::edit_git_config()
 git --version
+usethis::use_git()
+usethis::edit_git_config()
+install.packages("usethis")
+library("usethis")
+use_git_config(user.name = "DB-555", user.email = "ditipriya.bhar@gmail.com")
